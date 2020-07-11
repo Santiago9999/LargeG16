6f1123f8c7ba13f671724e8786bd9f2793bb40e6
Juan Herrera <JuanHerrera413@hotmail.com>
2020-07-11 13:48:22 -0700

    HEML ME LZ

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.listen(PORT, function () {
 
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-app.get('*', (req, res) => 
-{
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
-});
+// app.get('*', (req, res) => 
+// {
+//   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+// });
